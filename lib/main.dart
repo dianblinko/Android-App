f2d3ca5a49534f8b50623f16b38b635dda3d4e13
dianblinko <chepurov.artem@gmail.com>
2022-02-05 22:33:36 +0500

    Гифки на весь экран, мелкие изменения.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:finteh/gif.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,29 +12,33 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Финтех',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const PostListPage(title: "Финтех",),
+      home: const GifListPage(title: "Финтех",),
     );
   }
 }
 
-class PostListPage extends StatefulWidget {
-  const PostListPage({Key? key, required this.title}) : super(key: key);
+class GifListPage extends StatefulWidget {
+  const GifListPage({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
-  _PostListPageState createState() => _PostListPageState();
+  _GifListPageState createState() => _GifListPageState();
 }
 
-class _PostListPageState extends StateMVC {
+class _GifListPageState extends StateMVC {
   late PostController _controller;
 
-  // передаем наш контроллер StateMVC конструктору и
+  // передаем контроллер StateMVC конструктору и
   // получаем на него ссылку
-  _PostListPageState() : super(PostController()) {
+  _GifListPageState() : super(PostController()) {
     _controller = controller as PostController;
   }
   // после инициализации состояния
@@ -117,13 +122,32 @@ class _PostListPageState extends StateMVC {
         return (
           Container(
             alignment: Alignment.topCenter,
+
             child: Column(
               children: [
-                CachedNetworkImage(
-                    imageUrl: gifURL,
-                    placeholder: (context, url) => const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => const Icon(Icons.error),
+                Row(
+                  children:[
+                    Expanded(
+                      child:
+                        CachedNetworkImage(
+                          imageUrl: gifURL,
+                          // placeholder: (context, url) => const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => const Icon(Icons.error),
+                          fit: BoxFit.fitHeight,
+                        )
+                    )
+                  ],
+
                 ),
+
+
+
+                // CachedNetworkImage(
+                //   imageUrl: gifURL,
+                //   placeholder: (context, url) => const CircularProgressIndicator(),
+                //   errorWidget: (context, url, error) => const Icon(Icons.error),
+                //   fit: BoxFit.fill,
+                // ),
                 const SizedBox(
                   height: 20,
                 ),
