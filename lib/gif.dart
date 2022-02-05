d414068a4af564e7c1031d0396848ab3c69e54b6
dianblinko <chepurov.artem@gmail.com>
2022-02-05 10:51:49 +0500

    Перед подключением MVC

--- lib/gif.dart
+++ lib/gif.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 class Gif{
@@ -79,15 +80,21 @@ class Repository {
 //       throw Exception("failed request");
 //     }
 //   }
+  //асинхронная функция
   Future<Gif> fetchGif() async {
+    print("Вызвали Rep.fetchGif");
     final url = Uri.parse("$SERVER");
+    print("Rep. запарсили url " + url.toString());
     // делаем GET запрос
     final response = await http.get(url);
+    print("сделали get запрос");
     if (response.statusCode == 200) {
       // если все ок то возвращаем посты
       // json.decode парсит ответ
+      print("Статус гет запроса = 200");
       return Gif.fromJson(json.decode(response.body));
     } else {
+      print("Статус гет запроса не равен 200");
       // в противном случае говорим об ошибке
       throw Exception("failed request");
     }
@@ -97,13 +104,13 @@ class Repository {
 
 
 
-class PostController {
+class PostController{
   // создаем наш репозиторий
   final Repository repo = new Repository();
 
   // конструктор нашего контроллера
   PostController();
-
+  //final url = 'https://i.gifer.com/VAyR.gif';
   // первоначальное состояние - загрузка данных
   GetResult currentState = GetResultLoading();
   void init() async {
@@ -112,6 +119,7 @@ class PostController {
       final gif = await repo.fetchGif();
       // если все ок то обновляем состояние на успешное
       currentState = GetResultSuccess(gif);
+      // setState(() => currentState = GetResultSuccess(gif));
     } catch (error) {
       // в противном случае произошла ошибка
       currentState = GetResultFailure("Нет интернета");
--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,6 @@ import 'package:finteh/gif.dart';
 
 void main() {
   runApp(const MyApp());
-  // runApp(
-  //   Text(
-  //     'Helen',
-  //     textDirection:  TextDirection.ltr,
-  //     textAlign: TextAlign.center,
-  //   ),
-  // );
 }
 
 class MyApp extends StatelessWidget {
@@ -48,15 +41,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  String URL = 'https://media1.giphy.com/media/CBKzbqaQxS7K331lil/giphy.gif?cid=ecf05e4790357d3e67ea05ad66715167ddc4f346b34f7dc4&rid=giphy.gif&ct=g';
+  // String URL = 'https://media1.giphy.com/media/CBKzbqaQxS7K331lil/giphy.gif?cid=ecf05e4790357d3e67ea05ad66715167ddc4f346b34f7dc4&rid=giphy.gif&ct=g';
+  PostController _controller = new PostController();
+
+  void initState(){
+    super.initState();
+    _controller.init();
+  }
+
+
   void _getNextPage(){
-    // PostController _controller;
-    // _controller = new PostController();
-    // _controller.init();
-    //
-    // setState(() {
-    //   URL = (_controller.currentState as GetResultSuccess).gif.gifURL;
-    // });
+    initState();
+
 
   }
 
@@ -66,68 +62,52 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // Этот вызов setState сообщает фреймворку Flutter, что в этом состоянии что-то
-      // изменилось, что заставляет его повторно запускать метод сборки ниже
-      // чтобы на дисплее отображались обновленные значения. Если бы мы изменили
-      // _counter без вызова setState(), тогда метод сборки не будет
-      // вызываться снова, и поэтому, похоже, ничего не произойдет.
-      _counter++;
-    });
-
-
-  }
-
   @override
   Widget build(BuildContext context) {
     // Этот метод запускается повторно при каждом вызове setState, например, как сделано
     // с помощью метода _incrementCounter, описанного выше.
-    //
-    // // Фреймворк Flutter был оптимизирован для повторного запуска методов сборки
-    // быстро, так что вы можете просто перестроить все, что нуждается в обновлении, а
-    // чем необходимость индивидуально изменять экземпляры виджетов.
     return Scaffold(
       appBar: AppBar(
         // Здесь мы берем значение из объекта MyHomePage, созданного методомppp
         // App.build, и используем его для установки заголовка панели приложений.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Центр - это виджет макета. Он берет одного ребенка и позиционирует его
-        // в середине родителя.
-        child: Column(
-          // Столбец также является виджетом макета. Он берет список дочерних элементов и
-          // упорядочивает их по вертикали. По умолчанию он настраивается в соответствии со своими
-          // дети горизонтально, и старается быть таким же высоким, как его родитель.
-          //
-          // // Вызовите "отладку рисования" (нажмите "p" в консоли, выберите действие
-          // "Переключить отладку рисования" в инспекторе Flutter в Android
-          // Studio или команда "Переключить отладочную краску" в коде Visual Studio)
-          // чтобы увидеть каркас для каждого виджета.
-          //
-          // Столбец имеет различные свойства, позволяющие управлять его размерами и
-          // расположением дочерних элементов. Здесь мы используем mainAxisAlignment для
-          // центрирования дочерних элементов по вертикали; основная ось здесь - вертикальная
-          // ось, потому что столбцы вертикальные (поперечная ось будет
-          // горизонтальной).
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Image.network(
-                  URL,
-                  // "http://static.devli.ru/public/images/gifs/201404/ee0e90eb-29d9-45a5-b748-865fa865c82d.gif",
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      // body: Center(
+      //   // Центр - это виджет макета. Он берет одного ребенка и позиционирует его
+      //   // в середине родителя.
+      //   child: Column(
+      //     // Столбец также является виджетом макета. Он берет список дочерних элементов и
+      //     // упорядочивает их по вертикали. По умолчанию он настраивается в соответствии со своими
+      //     // дети горизонтально, и старается быть таким же высоким, как его родитель.
+      //     //
+      //     // // Вызовите "отладку рисования" (нажмите "p" в консоли, выберите действие
+      //     // "Переключить отладку рисования" в инспекторе Flutter в Android
+      //     // Studio или команда "Переключить отладочную краску" в коде Visual Studio)
+      //     // чтобы увидеть каркас для каждого виджета.
+      //     //
+      //     // Столбец имеет различные свойства, позволяющие управлять его размерами и
+      //     // расположением дочерних элементов. Здесь мы используем mainAxisAlignment для
+      //     // центрирования дочерних элементов по вертикали; основная ось здесь - вертикальная
+      //     // ось, потому что столбцы вертикальные (поперечная ось будет
+      //     // горизонтальной).
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     mainAxisSize: MainAxisSize.max,
+      //     children: <Widget>[
+      //       Image.network(
+      //             URL,
+      //             // "http://static.devli.ru/public/images/gifs/201404/ee0e90eb-29d9-45a5-b748-865fa865c82d.gif",
+      //       ),
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headline4,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: _buildContent(),
       floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           // mainAxisSize: MainAxisSize.max,
@@ -157,4 +137,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
     );
   }
+  Widget _buildContent() {
+    // первым делом получаем текущее состояние
+    final state = _controller.currentState;
+    if (state is GetResultLoading) {
+      // загрузка
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is GetResultFailure) {
+      // ошибка
+      return Center(
+        child: Text(
+            state.error,
+            textAlign: TextAlign.center,
+            //style: Theme.of(context).textTheme.headline4.copyWith(color: Colors.red)
+        ),
+      );
+    } else {
+      // отображаем список постов
+      final posts = (state as GetResultSuccess).gif.gifURL;
+      return Image.network(posts);
+      // return Padding(
+      //   padding: EdgeInsets.all(10),
+      //   // ListView.builder создает элемент списка
+      //   // только когда он видим на экране
+      //   child: ListView.builder(
+      //     itemCount: posts.length,
+      //     itemBuilder: (context, index) {
+      //       return _buildPostItem(posts[index]);
+      //     },
+      //   ),
+      // );
+    }
+  }
+
 }
